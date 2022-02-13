633caab7d9f351440af77d1821cf619ff870e48b
iampapaisarkar <iampapaisarkar@gmail.com>
2022-02-13 19:55:54 +0530

    product & services done

--- app/Http/Controllers/Vendor/RegistrationController.php
+++ app/Http/Controllers/Vendor/RegistrationController.php
@@ -27,7 +27,6 @@ class RegistrationController extends Controller
         $companyDetails = CompanyDetails::where('user_id', $authUser->id)->first();
         $companyDirectors = CompanyDirectors::where('user_id', $authUser->id)->get();
         $productServices = ProductServices::where('user_id', $authUser->id)->get();
-
         $serviceTypes = [];
         $services = [];
         foreach ($productServices as $key => $productService) {
@@ -36,128 +35,9 @@ class RegistrationController extends Controller
             }
             array_push($services, $productService->service_id);
         }
+        $categoryDocuments = CategoryDocuments::where('user_id', $authUser->id)->first();
 
-        // dd(['service types' => $serviceTypes, 'services' => $services]);
-        return view('vendor-user.registration', compact('companyDetails', 'companyDirectors', 'serviceTypes', 'services'));
-    }
-
-    // public function registrationPreview(RegistrationStoreRequest $request)
-    // {
-    //     if($request->all()){
-    //         $data = $request->all();
-    //         $data['attachment_9'] = base64_encode($request->file('attachment_9'));
-    //         return view('vendor-user.registration-preview', compact('data'));
-    //     }else{
-    //         return view('vendor-user.registration');
-    //     }
-
-    // }
-
-    public function registrationSubmit(RegistrationStoreRequest $request)
-    {
-        try {
-            DB::beginTransaction();
-
-            $registration = Registration::create([
-                'user_id' => Auth::user()->id,
-                'payment' => false,
-                'status' => 'pending',
-                'type' => 'vendor_registration'
-            ]);
-
-            CompanyDetails::create([
-                'registration_id' => $registration->id,
-                'area_of_core_competence' => $request->area_of_core_competence,
-                'type_of_organization' => $request->type_of_organization,
-                'company_name' => $request->company_name,
-                'cac_number' => $request->cac_number,
-                'default' => $request->default,
-                'share_capital' => $request->share_capital,
-                'address' => $request->address,
-                'landmark' => $request->landmark,
-                'city' => $request->city,
-                'state' => $request->state,
-                'first_name' => $request->first_name,
-                'last_name' => $request->last_name,
-                'email' => $request->email,
-                'phone_number' => $request->phone_number,
-                'position' => $request->position,
-            ]);
-
-            if($request->director){
-                foreach($request->director as $key => $director) {
-
-                    $passport = FileUpload::upload($director['passport'], $private = true, 'vendor', 'passport'.$key);
-                    $identification = FileUpload::upload($director['identification'], $private = true, 'vendor', 'identification'.$key);
-                    $certificates = FileUpload::upload($director['certificates'], $private = true, 'vendor', 'certificates'.$key);
-
-                    CompanyDirectors::create([
-                        'registration_id' => $registration->id,
-                        'first_name' => $request->first_name,
-                        'last_name' => $request->last_name,
-                        'email' => $request->email,
-                        'phone_number' => $request->phone_number,
-                        'address' => $request->address,
-                        'passport' => $passport,
-                        'identification' => $identification,
-                        'certificates' => $certificates,
-                    ]);
-                }
-            }
-
-            // dd($request->all());
-
-            if($request->services){
-                foreach($request->services as $key => $service) {
-                    if($service['service_type']){
-                        $service_type_id = $service['service_type'];
-                    }
-                    foreach($service['service'] as $serv) {
-                        ProductServices::create([
-                            'registration_id' => $registration->id,
-                            'service_type_id' => $service_type_id,
-                            'service_id' => $serv
-                        ]);
-                    }
-                }
-            }
-
-            $attachment_1 = FileUpload::upload($request->file('attachment_1'), $private = true, 'vendor', 'attachment_1');
-            $attachment_2 = FileUpload::upload($request->file('attachment_2'), $private = true, 'vendor', 'attachment_2');
-            $attachment_3 = FileUpload::upload($request->file('attachment_3'), $private = true, 'vendor', 'attachment_3');
-            $attachment_4 = FileUpload::upload($request->file('attachment_4'), $private = true, 'vendor', 'attachment_4');
-            $attachment_5 = FileUpload::upload($request->file('attachment_5'), $private = true, 'vendor', 'attachment_5');
-            $attachment_6 = FileUpload::upload($request->file('attachment_6'), $private = true, 'vendor', 'attachment_6');
-            $attachment_7 = FileUpload::upload($request->file('attachment_7'), $private = true, 'vendor', 'attachment_7');
-            $attachment_8 = FileUpload::upload($request->file('attachment_8'), $private = true, 'vendor', 'attachment_8');
-            $attachment_9 = FileUpload::upload($request->file('attachment_9'), $private = true, 'vendor', 'attachment_9');
-
-            CategoryDocuments::create([
-                'registration_id' => $registration->id,
-                'registration_category_id' => $request->registration_category_id,
-                'attachment_1' => $attachment_1,
-                'attachment_2' => $attachment_2,
-                'attachment_3' => $attachment_3,
-                'attachment_4' => $attachment_4,
-                'attachment_5' => $attachment_5,
-                'attachment_6' => $attachment_6,
-                'attachment_7' => $attachment_7,
-                'attachment_8' => $attachment_8,
-                'attachment_9' => $attachment_9,
-            ]);
-           
-
-            $response = Checkout::checkout($registration = ['id' => $registration->id], 'vendor_registration');
-
-            DB::commit();
-
-                return redirect()->route('invoice.show', ['id' => $response['id']])
-                ->withSuccess('Application successfully submitted. Please pay amount for further action');
-
-        }catch(Exception $e) {
-            DB::rollback();
-            return back()->with('error','There something internal server errore');
-        }
+        return view('vendor-user.registration', compact('companyDetails', 'companyDirectors', 'serviceTypes', 'services', 'categoryDocuments'));
     }
 
     public function status(){
@@ -431,5 +311,138 @@ class RegistrationController extends Controller
 
     public function registrationCategoryDocumentSubmit(CategoryDocumentRequest $request)
     {
+        try {
+            DB::beginTransaction();
+
+            $authUser = Auth::user();
+            $CategoryDocuments = CategoryDocuments::where('user_id', $authUser->id)
+            ->where('registration_id', null);
+
+            if($CategoryDocuments->exists()){
+
+                if($request->file('attachment_1')){
+                    $attachment_1 = FileUpload::upload($request->file('attachment_1'), $private = true, 'vendor', 'attachment_1');
+                }else{
+                    $attachment_1 = $CategoryDocuments->first()->attachment_1;
+                }
+                if($request->file('attachment_2')){
+                    $attachment_2 = FileUpload::upload($request->file('attachment_2'), $private = true, 'vendor', 'attachment_2');
+                }else{
+                    $attachment_2 = $CategoryDocuments->first()->attachment_2;
+                }
+                if($request->file('attachment_3')){
+                    $attachment_3 = FileUpload::upload($request->file('attachment_3'), $private = true, 'vendor', 'attachment_3');
+                }else{
+                    $attachment_3 = $CategoryDocuments->first()->attachment_3;
+                }
+                if($request->file('attachment_4')){
+                    $attachment_4 = FileUpload::upload($request->file('attachment_4'), $private = true, 'vendor', 'attachment_4');
+                }else{
+                    $attachment_4 = $CategoryDocuments->first()->attachment_4;
+                }
+                if($request->file('attachment_5')){
+                    $attachment_5 = FileUpload::upload($request->file('attachment_5'), $private = true, 'vendor', 'attachment_5');
+                }else{
+                    $attachment_5 = $CategoryDocuments->first()->attachment_5;
+                }
+                if($request->file('attachment_6')){
+                    $attachment_6 = FileUpload::upload($request->file('attachment_6'), $private = true, 'vendor', 'attachment_6');
+                }else{
+                    $attachment_6 = $CategoryDocuments->first()->attachment_6;
+                }
+                if($request->file('attachment_7')){
+                    $attachment_7 = FileUpload::upload($request->file('attachment_7'), $private = true, 'vendor', 'attachment_7');
+                }else{
+                    $attachment_7 = $CategoryDocuments->first()->attachment_7;
+                }
+                if($request->file('attachment_8')){
+                    $attachment_8 = FileUpload::upload($request->file('attachment_8'), $private = true, 'vendor', 'attachment_8');
+                }else{
+                    $attachment_8 = $CategoryDocuments->first()->attachment_8;
+                }
+                if($request->file('attachment_9')){
+                    $attachment_9 = FileUpload::upload($request->file('attachment_9'), $private = true, 'vendor', 'attachment_9');
+                }else{
+                    $attachment_9 = $CategoryDocuments->first()->attachment_9;
+                }
+
+                CategoryDocuments::where('user_id', $authUser->id)->update([
+                    'user_id' => $authUser->id,
+                    'registration_category_id' => $request->registration_category_id,
+                    'attachment_1' => $attachment_1,
+                    'attachment_2' => $attachment_2,
+                    'attachment_3' => $attachment_3,
+                    'attachment_4' => $attachment_4,
+                    'attachment_5' => $attachment_5,
+                    'attachment_6' => $attachment_6,
+                    'attachment_7' => $attachment_7,
+                    'attachment_8' => $attachment_8,
+                    'attachment_9' => $attachment_9,
+                ]);
+
+                $message = 'Category & Documents updated successfully';
+            }else{
+
+                $attachment_1 = FileUpload::upload($request->file('attachment_1'), $private = true, 'vendor', 'attachment_1');
+                $attachment_2 = FileUpload::upload($request->file('attachment_2'), $private = true, 'vendor', 'attachment_2');
+                $attachment_3 = FileUpload::upload($request->file('attachment_3'), $private = true, 'vendor', 'attachment_3');
+                $attachment_4 = FileUpload::upload($request->file('attachment_4'), $private = true, 'vendor', 'attachment_4');
+                $attachment_5 = FileUpload::upload($request->file('attachment_5'), $private = true, 'vendor', 'attachment_5');
+                $attachment_6 = FileUpload::upload($request->file('attachment_6'), $private = true, 'vendor', 'attachment_6');
+                $attachment_7 = FileUpload::upload($request->file('attachment_7'), $private = true, 'vendor', 'attachment_7');
+                $attachment_8 = FileUpload::upload($request->file('attachment_8'), $private = true, 'vendor', 'attachment_8');
+                $attachment_9 = FileUpload::upload($request->file('attachment_9'), $private = true, 'vendor', 'attachment_9');
+               
+                CategoryDocuments::create([
+                    'user_id' => $authUser->id,
+                    'registration_category_id' => $request->registration_category_id,
+                    'attachment_1' => $attachment_1,
+                    'attachment_2' => $attachment_2,
+                    'attachment_3' => $attachment_3,
+                    'attachment_4' => $attachment_4,
+                    'attachment_5' => $attachment_5,
+                    'attachment_6' => $attachment_6,
+                    'attachment_7' => $attachment_7,
+                    'attachment_8' => $attachment_8,
+                    'attachment_9' => $attachment_9,
+                ]);
+
+                $message = 'Category & Documents stored successfully';
+            }
+
+        DB::commit();
+
+            return response()->json($message, 200);
+
+
+        }catch(Exception $e) {
+            DB::rollback();
+            return response()->json('error','There something internal server errore');
+        }
+    }
+
+    public function registrationSubmit(RegistrationStoreRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $registration = Registration::create([
+                'user_id' => Auth::user()->id,
+                'payment' => false,
+                'status' => 'pending',
+                'type' => 'vendor_registration'
+            ]);
+
+            $response = Checkout::checkout($registration = ['id' => $registration->id], 'vendor_registration');
+
+            DB::commit();
+
+                return redirect()->route('invoice.show', ['id' => $response['id']])
+                ->withSuccess('Application successfully submitted. Please pay amount for further action');
+
+        }catch(Exception $e) {
+            DB::rollback();
+            return back()->with('error','There something internal server errore');
+        }
     }
 }
--- resources/views/vendor-user/forms/form4.blade.php
+++ resources/views/vendor-user/forms/form4.blade.php
@@ -1,3 +1,5 @@
+<form id="vendorRegistrationForm4">
+@csrf
 <div id="social-links-vertical-modern" class="content" role="tabpanel"
     aria-labelledby="social-links-vertical-modern-trigger">
     <div class="content-header">
@@ -13,7 +15,7 @@
             <select name="registration_category_id" class="form-select @error('registration_category_id') is-invalid @enderror" id="basicSelect">
                 <option value="">select one</option>
                 @foreach(app('App\Http\Services\BackendData')->RegistrationCategories() as $RegistrationCategory)
-                @if(old('registration_category_id') && (old('registration_category_id') == $RegistrationCategory->id))
+                @if((old('registration_category_id') && (old('registration_category_id') == $RegistrationCategory->id)) || ($categoryDocuments && $categoryDocuments->registration_category_id == $RegistrationCategory->id))
                 <option value="{{$RegistrationCategory->id}}" selected>{{$RegistrationCategory->title}}</option>
                 @endif
                 <option value="{{$RegistrationCategory->id}}">{{$RegistrationCategory->title}}</option>
@@ -34,7 +36,7 @@
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">Application Letter with Company Letterhead for Registration
                 addressed to the General Manager (PPA) (PDF/2MB max)</label>
-            <input name="attachment_1" class="form-select @error('attachment_1') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_1" class="form-select @error('attachment_1') is-invalid @enderror" type="file" id="formFile1" accept="application/pdf" />
             @error('attachment_1')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -47,7 +49,7 @@
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">Certificate of Incorporation/Registration of Business Name (CAC
                 Certificate) (PDF/2MB max)</label>
-            <input name="attachment_2" class="form-select @error('attachment_2') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_2" class="form-select @error('attachment_2') is-invalid @enderror" type="file" id="formFile2" accept="application/pdf" />
             @error('attachment_2')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -59,7 +61,7 @@
         </div>
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">CAC Form 1.1: (Statement of Share Capital) (PDF/2MB max)</label>
-            <input name="attachment_3" class="form-select @error('attachment_3') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_3" class="form-select @error('attachment_3') is-invalid @enderror" type="file" id="formFile3" accept="application/pdf" />
             @error('attachment_3')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -72,7 +74,7 @@
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">CAC Form 7/7A: (Particulars of First Director/Notice of Change of
                 Directors\ for Business Name, Particulars of Proprietorship) (PDF/2MB max)</label>
-            <input name="attachment_4" class="form-select @error('attachment_4') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_4" class="form-select @error('attachment_4') is-invalid @enderror" type="file" id="formFile4" accept="application/pdf" />
             @error('attachment_4')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -85,7 +87,7 @@
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">Copy of Company Memorandum & Articles of Association (PDF/2MB
                 max)</label>
-            <input name="attachment_5" class="form-select @error('attachment_5') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_5" class="form-select @error('attachment_5') is-invalid @enderror" type="file" id="formFile5" accept="application/pdf" />
             @error('attachment_5')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -99,7 +101,7 @@
             <label for="formFile" class="form-label">Current 3 years Personal-Katsina State Government Tax Clearance
                 Certificate as well as Development Levy of Chief Executive Officer and One (1) Director. (In case of
                 Business Names One(1) Director is sufficient) (PDF/2MB max)</label>
-            <input name="attachment_6" class="form-select @error('attachment_6') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_6" class="form-select @error('attachment_6') is-invalid @enderror" type="file" id="formFile6" accept="application/pdf" />
             @error('attachment_6')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -113,7 +115,7 @@
             <label for="formFile" class="form-label">Company Profile detailing the company's structure, Key personnel
                 Supported with Professional Licenses/Certificates, curriculum Vitae and Similar jobs executed in the
                 past (PDF/2MB max)</label>
-            <input name="attachment_7" class="form-select @error('attachment_7') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_7" class="form-select @error('attachment_7') is-invalid @enderror" type="file" id="formFile7" accept="application/pdf" />
             @error('attachment_7')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -126,7 +128,7 @@
         <div class="col-lg-12 col-md-12 mb-3 mb-sm-0">
             <label for="formFile" class="form-label">Curriculum Vitae of key staff supported by Professional
                 Licenses/Certificates (PDF/2MB max)</label>
-            <input name="attachment_8" class="form-select @error('attachment_8') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_8" class="form-select @error('attachment_8') is-invalid @enderror" type="file" id="formFile8" accept="application/pdf" />
             @error('attachment_8')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -140,7 +142,7 @@
             <label for="formFile" class="form-label">Applicants Managing Director/Chief Executive Officer's Declaration
                 on oath as to the authenticity of all submitted documents and engagement of professionals (PDF/2MB
                 max)</label>
-            <input name="attachment_9" class="form-select @error('attachment_9') is-invalid @enderror" type="file" id="formFile" accept="application/pdf" />
+            <input name="attachment_9" class="form-select @error('attachment_9') is-invalid @enderror" type="file" id="formFile9" accept="application/pdf" />
             @error('attachment_9')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -155,6 +157,70 @@
             <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
             <span class="align-middle d-sm-inline-block d-none">Previous</span>
         </button>
-        <button type="submit" class="btn btn-success btn-submit">Submit</button>
+        <button id="formSubmit4" class="btn btn-primary d-flex justify-content-between" type="submit"> 
+            <span class="align-middle d-sm-inline-block d-none">Submit</span>
+            <div id="form4Loader" style="width: 15px;height: 15px;" class="d-none spinner-border text-light ml-2" role="status"></div>
+        </button>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        var $form = $("#vendorRegistrationForm4");
+        $form.validate({
+            submitHandler: function(form) {
+                var formData = new FormData(form);
+                $("#form4Next").addClass('d-none');
+                $("#form4Loader").removeClass('d-none');
+                $("#formSubmit4").attr('disabled', true);
+                $.ajax({
+                    url: "<?php echo asset('') ?>"+"registration-category-document-submit",
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    type: 'POST',
+                    data: formData,
+                    dataType: "json",
+                    success: function(response) {
+                        $("#form4Next").removeClass('d-none');
+                        $("#form4Loader").addClass('d-none');
+                        $("#formSubmit4").attr('disabled', false);
+
+                        setTimeout(function() {
+                            toastr['success'](
+                                response, {
+                                    closeButton: true,
+                                    tapToDismiss: false
+                                }
+                            );
+                        }, 1000);
+                    },
+                    error: function(errors){
+
+                        if(errors.status == 422){
+                            var errorMessages = errors.responseJSON.errors
+                            var validator = $( "#vendorRegistrationForm4" ).validate();
+                            validator.showErrors(errorMessages);
+                        }
+
+                        $("#form4Next").removeClass('d-none');
+                        $("#form4Loader").addClass('d-none');
+                        $("#formSubmit4").attr('disabled', false);
+
+                        setTimeout(function() {
+                            toastr['error'](
+                                errors.responseJSON.message, {
+                                    closeButton: true,
+                                    tapToDismiss: false
+                                }
+                            );
+                        }, 1000);
+
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                });
+                return false;
+            }
+        });
+    });
+</script>
