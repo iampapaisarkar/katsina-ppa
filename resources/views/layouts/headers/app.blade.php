5ca7f09a1d638529197b83d9dc86e21669184c3c
iampapaisarkar <iampapaisarkar@gmail.com>
2022-02-14 10:43:51 +0530

    avatar change

--- resources/views/layouts/headers/app.blade.php
+++ resources/views/layouts/headers/app.blade.php
@@ -34,7 +34,7 @@
                 
                 
                 <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{Auth::user()->first_name}} {{Auth::user()->sur_name}}</span><span class="user-status">{{Auth::user()->role->role}}</span></div><span class="avatar"><img class="round" src="{{ asset('libs/app-assets/images/portrait/small/avatar-s-11.jpg')}}" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                        <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{Auth::user()->first_name}} {{Auth::user()->sur_name}}</span><span class="user-status">{{Auth::user()->role->role}}</span></div><span class="avatar"><img class="round" src="{{ asset('libs/app-assets/images/avatars/avatar.jpeg')}}" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
                     
